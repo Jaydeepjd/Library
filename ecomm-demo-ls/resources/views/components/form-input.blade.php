0073d4ed7d63b4ca1b04a45ecd0996a2371af916
Jaydeepjd <dhokaijaydeep@gmail.com>
2026-08-17 23:13:40 +0530

    Delete ecomm-demo-ls directory

--- ecomm-demo-ls/resources/views/components/form-input.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['type' => 'text', 'name' => '', 'label' => ''])
-
-<div>
-    @if ($label)
-        <label for="{{ $name }}" class="mb-2 block text-sm font-medium text-slate-700">{{ $label }}</label>
-    @endif
-    <input type="{{ $type }}" name="{{ $name }}" id="{{ $name }}" {{ $attributes->merge(['class' => 'w-full rounded-xl border border-slate-300 px-4 py-3 text-sm text-slate-900 shadow-sm focus:border-slate-500 focus:outline-none']) }} />
-</div>
